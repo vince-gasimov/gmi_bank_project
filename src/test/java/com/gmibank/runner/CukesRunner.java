d7609d3eddd35961368ef2c22309e774a23027ad
mstf <ursn.mustafa@gmail.com>
2020-12-07 19:51:28 +0100

    Add UserInfoPage1

--- src/test/java/com/gmibank/runner/CukesRunner.java
+++ src/test/java/com/gmibank/runner/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/gmibank/stepDefinitions",
-        tags = "@us06",
+        tags = "@wip",
         dryRun = true
 )
 public class CukesRunner {
