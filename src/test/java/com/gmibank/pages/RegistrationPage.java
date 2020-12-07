cf4b9699e4144307025fdf02e6e3754d7caa1085
EmreCihanbeyoglu <mrcihanbeyoglu@gmail.com>
2020-12-07 12:56:16 +0100

    RegistrationPage1

--- src/test/java/com/gmibank/pages/RegistrationPage.java
+++ src/test/java/com/gmibank/pages/RegistrationPage.java
@@ -1,9 +1,16 @@
 package com.gmibank.pages;
 
+
+import com.gmibank.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class RegistrationPage extends BasePage{
+import java.util.List;
+import java.util.Map;
+
+public class RegistrationPage extends BasePage {
 
 /*
     Abla bu kismi BasePage sayfasinda yaptik. RegistrationPage sayfasini da
@@ -23,8 +30,8 @@ public class RegistrationPage extends BasePage{
     public WebElement registerMenuLink;*/
 
 /*
-    bu locator'a beraber bakabiliriz. Benim anladigim kadariyla bu uyari mesaji icin kullanilacak. Uyari mesajlarinin hepsi icin bir tane
-    List<WebElement> tanimlayabiliriz. Hepsini bunun icinden kontrol edebiliriz. Bakalim berbaer ins.
+    bu locator'a beraber bakabiliriz. Benim anladigim kadariyla bu uyari mesaji icin kullanilacak.
+    Bunlarin text'leri degisebiliyor, o yuzden biraz daha dynamic yapmakta fayda var
 
     @FindBy(xpath = "//div[.='Your SSN is invalid']")
     public WebElement ssnHataMesaji;*/
@@ -52,34 +59,155 @@ public class RegistrationPage extends BasePage{
     @FindBy(id = "ssn")
     public WebElement ssnTextBox;
 
+    @FindBy(xpath = "//input[@id='ssn']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement ssnErrorMessage;
+
+
     @FindBy(id = "firstname")
     public WebElement firstNameTextBox;
+
+    @FindBy(xpath = "//input[@id='firstname']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement firstNameErrorMessage;
+
 
     @FindBy(id = "lastname")
     public WebElement lastNameTextBox;
 
+    @FindBy(xpath = "//input[@id='lastname']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement lastNameErrorMessage;
+
+
     @FindBy(id = "address")
     public WebElement addressTextBox;
+
 
     @FindBy(id = "mobilephone")
     public WebElement mobilePhoneTextBox;
 
+    @FindBy(xpath = "//input[@id='mobilephone']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement mobilePhoneErrorMessage;
+
+
     @FindBy(id = "username")
     public WebElement userNameTextBox;
+
+    @FindBy(xpath = "//input[@id='username']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement userNameErrorMessage;
+
 
     @FindBy(id = "email")
     public WebElement emailTextBox;
 
+    @FindBy(xpath = "//input[@id='email']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement emailErrorMessage;
+
+
     @FindBy(id = "firstPassword")
     public WebElement newPasswordTextBox;
+
+    @FindBy(xpath = "//input[@id='firstPassword']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement newPasswordErrorMessage;
+
 
     @FindBy(id = "secondPassword")
     public WebElement passwordConfirmationTextBox;
 
+    @FindBy(xpath = "//input[@id='secondPassword']/following-sibling::div[@class='invalid-feedback']")
+    public WebElement passwordConfirmationErrorMessage;
+
+
     @FindBy(id = "register-submit")
     public WebElement registerButton;
 
+    public void clickRegisterButton() {
+        registerButton.click();
+    }
+
+    public void typeSsnNumber(String ssnNumber) {
+        ssnTextBox.click();
+        ssnTextBox.sendKeys(ssnNumber);
+        ssnTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeFirstName(String firstName) {
+        firstNameTextBox.click();
+        firstNameTextBox.sendKeys(firstName);
+        firstNameTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeLastName(String lastName) {
+        lastNameTextBox.click();
+        lastNameTextBox.sendKeys(lastName);
+        lastNameTextBox.sendKeys(Keys.TAB);
+    }
 
 
+    public void typeAddress(String address) {
+        addressTextBox.click();
+        addressTextBox.sendKeys(address);
+        addressTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeMobilePhoneNumber(String phoneNumber) {
+        mobilePhoneTextBox.click();
+        mobilePhoneTextBox.sendKeys(phoneNumber);
+        mobilePhoneTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeUserName(String userName) {
+        userNameTextBox.click();
+        userNameTextBox.sendKeys(userName);
+        userNameTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeEmail(String email) {
+        emailTextBox.click();
+        emailTextBox.sendKeys(email);
+        emailTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeNewPassword(String newPassword) {
+        newPasswordTextBox.click();
+        newPasswordTextBox.sendKeys(newPassword);
+        newPasswordTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeNewPasswordConfirmation(String newPasswordConfirmation) {
+        passwordConfirmationTextBox.click();
+        passwordConfirmationTextBox.sendKeys(newPasswordConfirmation);
+        passwordConfirmationTextBox.sendKeys(Keys.TAB);
+    }
+
+    public void typeAllFieldInformation(Map<String, String> fieldsValuesMap) {
+        typeSsnNumber(fieldsValuesMap.get("ssnNumber"));
+        typeFirstName(fieldsValuesMap.get("firstName"));
+        typeLastName(fieldsValuesMap.get("lastName"));
+        typeAddress(fieldsValuesMap.get("address"));
+        typeMobilePhoneNumber(fieldsValuesMap.get("mobilePhoneNumber"));
+        typeUserName(fieldsValuesMap.get("userName"));
+        typeEmail(fieldsValuesMap.get("email"));
+        typeNewPassword(fieldsValuesMap.get("newPassword"));
+        typeNewPasswordConfirmation(fieldsValuesMap.get("passwordConfirmation"));
+    }
+
+    public void registerNewUser(Map<String, String> fieldsValuesMap){
+        typeAllFieldInformation(fieldsValuesMap);
+        if (!isThereAnyError()){
+            registerButton.click();
+        }else{
+            System.out.println("There is error inside some textboxes");
+        }
+
+    }
+
+    public boolean isThereAnyError(){
+        List<WebElement> errorWebElements = Driver.getDriver().findElements(By.cssSelector("invalid-feedback"));
+        int size = errorWebElements.size();
+        if (size == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
 
 }
---
+++ src/test/java/com/gmibank/stepDefinitions/TestClass.java
@@ -0,0 +1,58 @@
+package com.gmibank.stepDefinitions;
+
+import com.gmibank.pages.BasePage;
+import com.gmibank.pages.RegistrationPage;
+import com.gmibank.utilities.BrowserUtils;
+import com.gmibank.utilities.ConfigurationReader;
+import com.gmibank.utilities.Driver;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestClass {
+
+    @Test
+    public void test() throws Exception {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        Driver.getDriver().manage().window().maximize();
+        BasePage basePage = new BasePage();
+        basePage.clickAndSelectDropDownItemUnderAccountMenuIcon("Register");
+        BrowserUtils.waitFor(2);
+        //System.out.println("basePage.getPageHeader() = " + basePage.getPageHeader());
+        RegistrationPage registrationPage = new RegistrationPage();
+        BrowserUtils.waitFor(1);
+
+        Map<String, String> values = new HashMap<>();
+        values.put("ssnNumber", "000-10-1111");
+        values.put("firstName", "emre");
+        values.put("lastName", "cihan");
+        values.put("address", "svdfd");
+        values.put("mobilePhoneNumber", "111-111-1111");
+        values.put("userName", "11111111");
+        values.put("email", "mr@gmail.com");
+        values.put("newPassword", "11111111");
+        values.put("passwordConfirmation", "11111111");
+
+        registrationPage.registerNewUser(values);
+
+
+/*
+
+        typeSsnNumber(fieldsValuesMap.get("ssnNumber"));
+        typeFirstName(fieldsValuesMap.get("firstName"));
+        typeLastName(fieldsValuesMap.get("lastName"));
+        typeAddress(fieldsValuesMap.get("address"));
+        typeMobilePhoneNumber(fieldsValuesMap.get("mobilePhoneNumber"));
+        typeUserName(fieldsValuesMap.get("userName"));
+        typeEmail(fieldsValuesMap.get("email"));
+        typeNewPassword(fieldsValuesMap.get("newPassword"));
+        typeNewPasswordConfirmation(fieldsValuesMap.get("passwordConfirmation"));
+*/
+
+        Driver.closeDriver();
+
+
+    }
+
+}
