dde173a956e10ef3c61413b72791ef053f20602b
mstf <ursn.mustafa@gmail.com>
2020-12-11 20:28:36 +0100

    Add UserinfoFriday

--- src/test/java/com/gmibank/pages/UserInfoPage.java
+++ src/test/java/com/gmibank/pages/UserInfoPage.java
@@ -1,6 +1,7 @@
 package com.gmibank.pages;
 
 import com.gmibank.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -8,7 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class UserInfoPage  {
+public class UserInfoPage {
 
     public UserInfoPage() {
 
@@ -17,7 +18,7 @@ public class UserInfoPage  {
 
 
     //sag ust kosedeki kullanici ikonu
-    @FindBy (id = "account-menu")
+    @FindBy(id = "account-menu")
     public WebElement accountMenuIcon;
 
     @FindBy(css = "[id='account-menu'] a[class='dropdown-item']")
@@ -40,7 +41,19 @@ public class UserInfoPage  {
     public WebElement saveButton;
 
 
+    @FindBy(xpath = "//div[@class='invalid-feedback']")
+    public List<WebElement> invalidEmailToAlert;
+
+    public void getAlert() {
+        List<WebElement> elements = Driver.getDriver().findElements(By.xpath("invalid-feedback"));
+
+        System.out.println(elements);
 
 
-}
+        if(elements.size()!=0){
+            System.out.println(elements.get(0));
+        }
 
+
+
+}}
