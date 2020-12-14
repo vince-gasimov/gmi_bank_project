0f9299b404969fd1511160970a6674100c785c4c
ismailozkan93 <ismailozkan93@icloud.com>
2020-12-14 22:28:45 +0100

    Deleted denemeclass

---
+++ src/test/java/com/gmibank/stepDefinitions/TestInfo2.java
@@ -0,0 +1,31 @@
+package com.gmibank.stepDefinitions;
+
+import com.gmibank.pages.TestInfo;
+import io.cucumber.java.en.When;
+
+public class TestInfo2 {
+
+    @When("user types a email without {string}")
+    public void user_types_a_email_without(String emailwithoutat) {
+
+        TestInfo testInfo=new TestInfo();
+        testInfo.emailKutusu.sendKeys(emailwithoutat);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
