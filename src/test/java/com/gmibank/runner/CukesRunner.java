bb8a439cd7c579ef5dd51f86d17f28347d91db39
Kadir <kadirkayaoglu@gmail.com>
2020-12-18 19:07:30 +0100

    API calismalari

--- src/test/java/com/gmibank/runner/CukesRunner.java
+++ src/test/java/com/gmibank/runner/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/gmibank/stepDefinitions",
-        tags = "@AllCustomerApi",
+        tags = "@wip",
         dryRun = false
 )
 public class CukesRunner {
