e1e7e45ea59d2df8a72242e960389daa4964d591
mstf <ursn.mustafa@gmail.com>
2020-12-21 23:27:04 +0100

    datautil

--- src/test/java/com/gmibank/utilities/DateUtil.java
+++ src/test/java/com/gmibank/utilities/DateUtil.java
@@ -7,32 +7,35 @@ import java.util.Date;
 
 public class DateUtil {
     // MMMM dd, yyy  --> February 17, 2019
-    public static String todaysDate(){
+    public static String todaysDate() {
         String today = new SimpleDateFormat("MMMM dd, yyy").format(new Date());
         return today;
     }
-    // MM-dd-yy     --> 02-17-19
-    public static String todaysDate2(){
-        String today = new SimpleDateFormat("MM-dd-yy").format(new Date());
+
+    // MM-dd-yy     --> 02-17-1999
+    public static String todaysDate2() {
+        String today = new SimpleDateFormat("MM-dd-yyyy").format(new Date());
         return today;
     }
+
     // dd-MM-yyyy    --> 17-02-1990
-    public static String todaysDate3(){
+    public static String todaysDate3() {
         String today = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
         return today;
     }
+
     // yyyy-MM-dd hh:mm:ss   --> 2019-02-17 03:45:11
-    public static String todaysDate4(){
+    public static String todaysDate4() {
         String today = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
         return today;
     }
 
-    public static String todaysDate5(){
+    public static String todaysDate5() {
         String today = new SimpleDateFormat("yyyy-MM-dd hh:mm").format(new Date());
         return today;
     }
 
-    public static String todaysDate6(){
+    public static String todaysDate6() {
         String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
         return today;
     }
